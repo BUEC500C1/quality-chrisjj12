64edabafb2a1991e3da95b99b2edb1a5b31fa652
Christopher Jung <Chris@dhcp-wifi-8021x-155-41-64-73.bu.edu>
2020-01-28 16:43:48 -0500

    final code

--- arab_conv.py
+++ arab_conv.py
@@ -1,25 +1,28 @@
-def conv(num2):
+def conv(num):
 
     arab = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
     rom = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
     i = 0
     j = 0
     output = ''
-    if num2 > 0:
-        while num2 < arab[i]:
+    
+    if num > 4999 or num <= 0:
+        print('Number is not valid')
+    elif  5000 > num > 0:
+        while num < arab[i]:
             i = i + 1
-        while (num2 // arab[i]) > 0:
-            num3 = num2 // arab[i]
-            while (num2 - arab[i]) >= 0 and j < num3:
+        while (num // arab[i]) > 0:
+            num2 = num // arab[i]
+            while (num - arab[i]) >= 0 and j < num2:
                 output = output + rom[i]
                 j = j + 1
             j = 0
-            num2 = num2 - (num3*arab[i])
-            if num2 == 0:
+            num = num - (num2*arab[i])
+            if num == 0:
                 return output
                 break
             i = i + 1
-            while num2 < arab[i]:
+            while num < arab[i]:
                 i = i + 1
 
 
--- test_conv.py
+++ test_conv.py
@@ -2,4 +2,11 @@ import arab_conv
 
 def test_answer():
     assert arab_conv.conv(3) == "III"
+    assert arab_conv.conv(4178) == "MMMMCLXXVIII"
+    assert arab_conv.conv(25) == "XXV"
+    assert arab_conv.conv(2384) == "MMCCCLXXXIV"
+    assert arab_conv.conv(4999) == "MMMMCMXCIX"
+    assert arab_conv.conv(-3) == "Number is not valid"
+    assert arab_conv.conv(5000) == "Number is not valid"
+    assert arab_conv.conv(100) == "C"
 
