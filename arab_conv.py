32668a765833014d251baf83fa2fb832236f150c
Christopher Jung <Chris@dhcp-wifi-8021x-155-41-64-73.bu.edu>
2020-01-28 16:02:22 -0500

    python code finished

--- arab_conv.py
+++ arab_conv.py
@@ -3,19 +3,23 @@ def conv(num2, output):
     arab = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
     rom = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
     i = 0
-    print("Here")
+    j = 0
     if num2 > 0:
-        print("Here in if")
-        for x in range(num2 // arab[i]):
-            print("in for")
-            print(x)
-            y = mag(x)
-            output = output + rom[i]
-            num2 = num2 - (y*arab[i])
-            print(output)
+        while num2 < arab[i]:
+            i = i + 1
+        while (num2 // arab[i]) > 0:
+            num3 = num2 // arab[i]
+            while (num2 - arab[i]) >= 0 and j < num3:
+                output = output + rom[i]
+                j = j + 1
+            j = 0
+            num2 = num2 - (num3*arab[i])
             if num2 == 0:
                 print(output)
+                break
             i = i + 1
+            while num2 < arab[i]:
+                i = i + 1
     #return output
 
 
@@ -25,4 +29,12 @@ num2 = int(num)
 output = ''
 
 conv(num2, output)
+
+
+
+        
+        
+
+
+
 
